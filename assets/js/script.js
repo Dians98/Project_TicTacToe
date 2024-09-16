4f0fda099639375a4d3d9cdb4ee9afe87494f4c3
Diano <Diano.faniry@gmail.com>
2024-09-16 16:29:37 +0400

    First commit !

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+const gameboard = {
+
+}
+
+const playerOne = {
+    "name" : "Player 1",
+    "mark" :"X",
+    "moves" : [],
+    "score" : 0
+}
+
+const playerTwo = {
+    "name" : "Player 2",
+    "mark" :"O",
+    "moves" : [],
+    "score" : 0
+}
